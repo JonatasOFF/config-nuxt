6e3c1e8ea57ed39be0074d0dfb137e367feb2f63
Jonatas Oliveira <jonatas.ferreira@jumplabel.com.br>
2021-03-27 15:55:53 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    CHUPA MINHA PICA
+    <h1>{{ post }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $axios, params }) {
+    const post = await $axios.$get('https://www.google.com/')
+    return { post }
+  }
+}
+</script>
